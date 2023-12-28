6a7eb9921640545f58cb19297d425c7b627bd91b
Divine <divinegodwin172@gmail.com>
2023-12-28 11:25:28 +0000

    new

---
+++ app/Contact/page.jsx
@@ -0,0 +1,15 @@
+"use client"
+import {useState} from "react"
+
+const Contact = () =>{
+
+    return(
+        <>
+<div>
+    <h1>Contact Me </h1>
+</div>
+        </>
+    )
+}
+
+export default Contact;
