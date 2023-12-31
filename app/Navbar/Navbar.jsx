4e8bda661d496f95c95e32e9f8a9c063ce827cf2
Divine Agbasimelo <divinegodwin172@gmail.com>
2023-12-31 20:24:44 +0100

    svg adjusted

--- app/Navbar/Navbar.jsx
+++ app/Navbar/Navbar.jsx
@@ -25,15 +25,15 @@ export const Navbar = () => {
         className="nav-icon-icon"
           data-slot="icon"
           fill="none"
-          stroke-width="1.5"
+          strokeWidth="1.5"
           stroke="currentColor"
           viewBox="0 0 24 24"
           xmlns="http://www.w3.org/2000/svg"
           aria-hidden="true"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5"
           ></path>
         </svg>
