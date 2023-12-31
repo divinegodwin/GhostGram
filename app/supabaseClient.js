e62562126d03cfad065fa8c3b2e58f8705de0850
Divine Agbasimelo <divinegodwin172@gmail.com>
2023-12-31 19:11:21 +0100

    databse config

--- app/supabaseClient.js
+++ app/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = "https://dgzqniremyrphlbfabuz.supabase.co"
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRnenFuaXJlbXlycGhsYmZhYnV6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDM5NDg5MjMsImV4cCI6MjAxOTUyNDkyM30.A-Mn6fNLw7mu_j_pUrHVyUIFSGDPQypemxI4NIO57m0"
+const supabaseUrl = 'https://dgzqniremyrphlbfabuz.supabase.co'
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
