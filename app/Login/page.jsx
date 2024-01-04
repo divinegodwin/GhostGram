0d8bea68357cc5b284fc2e346da8c76ac9450443
Destiny Felix Kiisi <kiisifelix06@gmail.com>
2024-01-04 15:39:39 +0100

    fix/Major fix on the login page user interface

--- app/Login/page.jsx
+++ app/Login/page.jsx
@@ -4,99 +4,103 @@ import { useState, useEffect } from "react";
 import supabase from "../supabaseClient";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-  
+
 const Login = () => {
- 
-const router = useRouter()
+
+  const router = useRouter()
 
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState('')
   const [isLoggedIn, setIsLoggedIn] = useState('')//state for checking user logged in status
 
-  const handleLogin = async(e) => {
+  const handleLogin = async (e) => {
     e.preventDefault();
-   
-    if(!username || !password){
-        setError('Fill in all inputs')
-        return
+
+    if (!username || !password) {
+      setError('Fill in all inputs')
+      return
     }
-    if(password.length <=6){
+    if (password.length <= 6) {
       setError('password should be more than 6 characters')
       return;
-     }
-try{
-     const {data, error} = await supabase.from('Account')
-     .select('username, password')
-     .eq('username', username)
-  if((data[0].password !== password) && (data.username !==username)){
-      setError('invalid username or password')
-      return
-  }
+    }
+    try {
+      const { data, error } = await supabase.from('Account')
+        .select('username, password')
+        .eq('username', username)
+      if ((data[0].password !== password) && (data.username !== username)) {
+        setError('invalid username or password')
+        return
+      }
 
-     if(error){
-      setError('error occured at login page', error)
-      return
-     }else if(data.username === 'Divine' || 'divine'){
-      alert('welcome boss')
-      router.push('/Posts')
-     }else if(data.username === 'Divine' || 'divine'){
-      setError('')
-      setIsLoggedIn(true)//setting loged in state value to true once user account is found
+      if (error) {
+        setError('error occured at login page', error)
+        return
+      } else if (data.username === 'Divine' || 'divine') {
+        alert('welcome boss')
+        router.push('/Posts')
+      } else if (data.username === 'Divine' || 'divine') {
+        setError('')
+        setIsLoggedIn(true)//setting loged in state value to true once user account is found
         router.push('/Posts')// Navigate to home page if there is no error and user is found
-         }    
-    }catch(error){
-        console.log('try error occured at login page', error)
-        setError('user not found')
-      } 
-    } 
+      }
+    } catch (error) {
+      console.log('try error occured at login page', error)
+      setError('user not found')
+    }
+  }
   if (isLoggedIn) {
     // User is logged in, don't render the Login component
     return null;//This means the Login component won't be rendered
   }
 
   return (
-    <div>
+    <div className="grid place-items-center px-4 py-20 min-h-[100vh]">
       <form
         onSubmit={handleLogin}
-        className="auth-login-form rounded-lg placeholder:w-[80%] h-[500px] flex flex-col gap-6 ml-[2rem] mt-[4rem] shadow-lg"
+        className="rounded-lg placeholder:w-[80%] w-full max-w-[500px] py-10 flex flex-col gap-6 shadow-lg px-4 sm:px-8"
       >
-        <div className="login-heading ml-[7.6rem] pt-[2rem] font-bold text-2xl">Sign In</div>
+        <header className="grid place-items-center py-5">
+          <h1 className="font-bold text-2xl">Sign In</h1>
+        </header>
         {error && (
-  <div><p className='text-[red] pl-9'>{error}</p></div>
-)}
+          <div><p className='text-[red] pl-9'>{error}</p></div>
+        )}
 
-        <div className="inputs-login-container flex flex-col gap-6 ml-[3rem] mt-4">
+        <div className="flex flex-col gap-6">
           <input
             onChange={(e) => setUsername(e.target.value)}
-            className="w-[220px] h-[40px] border-4 p-6 rounded-lg outline-[#3f37c9]"
+            className="w-full h-[40px] border-4 p-6 rounded-lg outline-[#3f37c9]"
             type="text"
             placeholder="username"
             value={username}
           ></input>
 
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            className="w-[220px] h-[40px] border-4 p-6 rounded-lg outline-[#3f37c9]"
-            type="password"
-            placeholder="password > 6 chars"
-            value={password}
-          ></input>
-                  <button type="submit" className="w-[220px] p-2 h-[50px] text-white border-2 ml-1 rounded-lg bg-[#3f37c9]">
-          Submit
-        </button>
+          <fieldset>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full h-[40px] border-4 p-6 rounded-lg outline-[#3f37c9]"
+              type="password"
+              placeholder="password > 6 chars"
+              value={password}
+            />
+            <div className="flex justify-between pt-2">
+              <div>&nbsp;</div>
+              <p className="text-[#3f37c9] text-[15px]">Forgotten password ?</p>
+            </div>
+          </fieldset>
+
+          <button type="submit" className="w-full p-2 h-[50px] text-white border-2 rounded-lg bg-[#3f37c9]">
+            Submit
+          </button>
         </div>
 
-<div  className="forgotten-password">
-  <p className=" pl-[6rem] text-red-500 forgotten-password">forgotten password ?</p>
-</div>
-
-            
-        <div className="mt-2 ">
+        <div className="grid place-items-center">
           <Link href='/Account'>
-          <button className="create-account pl-[6rem] focus:text-[red] text-[grey] text-lg">Create an Account</button>
+            <button className="focus:text-[red] text-[grey] text-lg">Create an Account</button>
           </Link>
-          </div>
+        </div>
       </form>
     </div>
   );
