77d22e86b412c61a533aa3f89fbdea3f72162881
Divine Agbasimelo <divinegodwin172@gmail.com>
2024-01-05 12:38:17 +0100

    contact me adjustment

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -27,34 +27,34 @@ const Contact = () => {
         <h1 className="font-bold">Contact Me </h1>
       </div>
 
-      <form className="form flex flex-col mt-14 shadow-lg w-[90%] h-[450px] ml-[1rem] ">
-        <div className="form-item-container">
-          <div className="flex flex-col  ml-[3.3rem] mb-6">
-            <label for="name" className="ml-[1rem]">
+      <form className=" grid place-items-center px-4  mt-10 shadow-lg max-w-[90%] h-[450px] ml-4 pl-8 ">
+        <div className="">
+          <div className="flex flex-col mb-6">
+            <label for="name" className="pl-1">
               Name
             </label>
             <input
               type="email"
-              className=" input w-[250px] h-[40px] mt-2 border-2 rounded-lg p-4"
+              className="  w-[260px] h-[60px] mt-2 border-4 rounded-lg p-4"
               onChange={handleName}
               placeholder="input your name"
             ></input>
           </div>
 
-          <div className="flex flex-col  ml-[3.3rem] mb-6">
-            <label for="email" className="ml-[1rem]">
+          <div className="flex flex-col  mb-6">
+            <label for="email" className="pl-2">
               Email
             </label>
             <input
               type="text"
-              className="input w-[250px] h-[40px] mt-2 border-2 rounded-lg p-4"
+              className=" w-[260px] h-[60px] mt-2 border-4 rounded-lg p-4"
               onChange={handleEmail}
               placeholder="input your email"
             ></input>
           </div>
 
-          <div className="flex flex-col  ml-[3.3rem] mb-6">
-            <label for="email" className="ml-[1rem]">
+          <div className="flex flex-col mb-6">
+            <label for="email" className="pl-2">
               Message
             </label>
             <textarea
@@ -62,12 +62,11 @@ const Contact = () => {
               cols={20}
               rows={6}
               placeholder="Enter max 20 characters"
-              className="input w-[250px] h-[90px] mt-2 border-2 rounded-lg p-4"
+              className="input w-[260px] h-[90px] mt-2 border-4 rounded-lg p-4"
             ></textarea>
           </div>
-
-          <div>
-            <button className="send-btn w-[200px] h-[45px] mt-4 bg-[#3f37c9] text-white rounded-lg ml-[5rem]">
+          <div className="ml-1">
+            <button className=" w-[260px] h-[55px]  bg-[#3f37c9] text-white rounded-lg ">
               Send
             </button>
           </div>
