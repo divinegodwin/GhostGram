e590088d99c1f9f290ac4b74ea83197f957f7326
Divine Agbasimelo <divinegodwin172@gmail.com>
2024-01-05 11:58:39 +0100

    added buttom nav

--- app/Contact/page.jsx
+++ app/Contact/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 import { useState } from "react";
 import { Navbar } from "../Navbar/Navbar";
+import Footer from "../Footer";
 
 const Contact = () => {
   const [name, setName] = useState("");
@@ -27,52 +28,52 @@ const Contact = () => {
       </div>
 
       <form className="form flex flex-col mt-14 shadow-lg w-[90%] h-[450px] ml-[1rem] ">
-        
         <div className="form-item-container">
-        <div className="flex flex-col  ml-[3.3rem] mb-6">
-          <label for="name" className="ml-[1rem]">
-            Name
-          </label>
-          <input
-            type="email"
-            className=" input w-[250px] h-[40px] mt-2 border-2 rounded-lg p-4"
-            onChange={handleName}
-            placeholder="input your name"
-          ></input>
-        </div>
+          <div className="flex flex-col  ml-[3.3rem] mb-6">
+            <label for="name" className="ml-[1rem]">
+              Name
+            </label>
+            <input
+              type="email"
+              className=" input w-[250px] h-[40px] mt-2 border-2 rounded-lg p-4"
+              onChange={handleName}
+              placeholder="input your name"
+            ></input>
+          </div>
 
-        <div className="flex flex-col  ml-[3.3rem] mb-6">
-          <label for="email" className="ml-[1rem]">
-            Email
-          </label>
-          <input
-            type="text"
-            className="input w-[250px] h-[40px] mt-2 border-2 rounded-lg p-4"
-            onChange={handleEmail}
-            placeholder="input your email"
-          ></input>
-        </div>
+          <div className="flex flex-col  ml-[3.3rem] mb-6">
+            <label for="email" className="ml-[1rem]">
+              Email
+            </label>
+            <input
+              type="text"
+              className="input w-[250px] h-[40px] mt-2 border-2 rounded-lg p-4"
+              onChange={handleEmail}
+              placeholder="input your email"
+            ></input>
+          </div>
 
-        <div className="flex flex-col  ml-[3.3rem] mb-6">
-          <label for="email" className="ml-[1rem]">
-            Message
-          </label>
-          <textarea
-          onChange={handleMessage}
-            cols={20}
-            rows={6}
-            placeholder="Enter max 20 characters"
-            className="input w-[250px] h-[90px] mt-2 border-2 rounded-lg p-4"
-          ></textarea>
-        </div>
+          <div className="flex flex-col  ml-[3.3rem] mb-6">
+            <label for="email" className="ml-[1rem]">
+              Message
+            </label>
+            <textarea
+              onChange={handleMessage}
+              cols={20}
+              rows={6}
+              placeholder="Enter max 20 characters"
+              className="input w-[250px] h-[90px] mt-2 border-2 rounded-lg p-4"
+            ></textarea>
+          </div>
 
-        <div>
-          <button className="send-btn w-[200px] h-[45px] mt-4 bg-[#3f37c9] text-white rounded-lg ml-[5rem]">
-            Send
-          </button>
-        </div>
+          <div>
+            <button className="send-btn w-[200px] h-[45px] mt-4 bg-[#3f37c9] text-white rounded-lg ml-[5rem]">
+              Send
+            </button>
+          </div>
         </div>
       </form>
+<Footer />
     </>
   );
 };
---
+++ app/Footer.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+
+<div className='flex flex-row gap-12 pl-16 pt-2 py-1 w-full h-[40px] bg-[#3f37c9] text-white'>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            class="main-grid-item-icon"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+          </svg>
+        </div>
+
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            class="main-grid-item-icon"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <rect height="20" rx="5" ry="5" width="20" x="2" y="2" />
+            <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z" />
+            <line x1="17.5" x2="17.51" y1="6.5" y2="6.5" />
+          </svg>
+        </div>
+
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            class="main-grid-item-icon"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z" />
+          </svg>
+        </div>
+
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            class="main-grid-item-icon"
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+          >
+            <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z" />
+            <rect height="12" width="4" x="2" y="9" />
+            <circle cx="4" cy="4" r="2" />
+          </svg>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
